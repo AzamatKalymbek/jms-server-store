64f631c82af8d158188f4d10f922917bcfce003e
Kalymbek Azamat <azamat.kz946@gmail.com>
2019-10-01 11:31:20 +0600

    some change

--- gateway/src/main/java/kz/teamvictus/store/gateway/GatewayApplication.java
+++ gateway/src/main/java/kz/teamvictus/store/gateway/GatewayApplication.java
@@ -1,18 +1,20 @@
-package kz.teamvictus.store.core;
+package kz.teamvictus.store.gateway;
 
-
-import kz.teamvictus.store.core.config.ApplicationSwaggerConfig;
+import kz.teamvictus.store.gateway.config.ApplicationSwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
+@EnableConfigurationProperties
 @Import(ApplicationSwaggerConfig.class)
-public class CoreApplication {
+public class GatewayApplication {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(CoreApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
